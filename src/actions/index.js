2992fecf9257285f78e2400c8bb20e5cf3df75e1
Zach Yam <thepredestrian@gmail.com>
2017-06-24 23:42:59 -0400

    polished coininfo component

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,5 +10,5 @@ export {
   authorizeExchange,
   fetchTopCurrencies,
   fetchCoinSearchTerm,
-  addCoin
+  addCoin,
 };
--- src/components/CoinInfo/index.js
+++ src/components/CoinInfo/index.js
@@ -1,15 +1,35 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Col, Modal, FormGroup, ControlLabel, FormControl, FieldGroup } from 'react-bootstrap';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions/index';
+import { Row, Button, Col, Modal, FormGroup, ControlLabel, FormControl, FieldGroup } from 'react-bootstrap';
 
 class CoinInfo extends Component {
   constructor(props) {
     super(props);
-    this.state = { showModal: false };
+    this.state = { showModal: false,
+                   txn: '',
+                   coinAmount: '',
+                   coinCurrencyAmount: '',
+                   coinCurrency: 'USD',
+                };
 
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
+    this.setBought = this.setBought.bind(this);
+    this.setSold = this.setSold.bind(this);
+    this.updateCoinAmount = this.updateCoinAmount.bind(this);
+    this.updateCoinCurrency = this.updateCoinCurrency.bind(this);
+    this.updateCoinCurrencyAmount = this.updateCoinCurrencyAmount.bind(this);
+
+  };
+  setBought () {
+    this.setState({ txn: 'Bought' })
+  };
+
+  setSold () {
+    this.setState({ txn: 'Sold' })
   };
 
   close() {
@@ -18,6 +38,18 @@ class CoinInfo extends Component {
 
   open() {
     this.setState({ showModal: true });
+  };
+
+  updateCoinAmount(e) {
+    this.setState({ coinAmount: e.target.value });
+  };
+
+  updateCoinCurrency(e) {
+    this.setState({ coinCurrency: e.target.value });
+  };
+
+  updateCoinCurrencyAmount(e) {
+    this.setState({ coinCurrencyAmount: e.target.value });
   };
 
   render() {
@@ -31,6 +63,9 @@ class CoinInfo extends Component {
       );
     }
     const { coin } = this.props;
+    const btnStyle = {
+      marginRight:'10px'
+    }
     return (
       <div>
         <h1> {coin} </h1>
@@ -41,27 +76,72 @@ class CoinInfo extends Component {
             <Modal.Title>Add a {coin} transaction</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Button bsStyle="success">Buy</Button>
-            <Button bsStyle="danger">Sell</Button>
+            <Row>
+              <Col xs={6} md={6}>
+                <Button style={btnStyle} bsStyle="success" onClick={this.setBought}>Bought</Button>
+                <Button bsStyle="danger" onClick={this.setSold}>Sold</Button>
+              </Col>
+            </Row>
             <br/>
             <br/>
-            <FieldGroup
-              id="formControlsText"
-              type="text"
-              label="Amount"
-              placeholder={coin}
-            />
+            <Row>
+              <Col xs={6} md={6}>
+                <FieldGroup
+                  id="formControlsText"
+                  type="text"
+                  label={this.state.txn}
+                  placeholder={coin}
+                  value={this.state.coinAmount}
+                  onChange={this.updateCoinAmount}
+                />
+              </Col>
+              <Col xs={6} md={6}>
+                <FieldGroup
+                  id="formControlsText"
+                  type="text"
+                  label="for"
+                  placeholder="Amount"
+                  value={this.state.coinCurrencyAmount}
+                  onChange={this.updateCoinCurrencyAmount} />
+                  <FormControl
+                    componentClass="select"
+                    value={this.state.value} onChange={this.updateCoinCurrency}>
+                    <option value="USD">USD</option>
+                    <option value="Bitcoin">BTC</option>
+                    <option value="Ethereum">ETH</option>
+                  </FormControl>
+              </Col>
+            </Row>
             <br/>
             <FormGroup controlId="formControlsSelect">
-              <ControlLabel>Exchange</ControlLabel>
-              <FormControl componentClass="select" placeholder="select">
-                <option value="select">select</option>
-                <option value="other">...</option>
-              </FormControl>
+              <FieldGroup
+                id="formControlsText"
+                type="text"
+                label="Date"
+                placeholder="MM/DD/YY"
+              />
             </FormGroup>
+            <br/>
+            { this.state.coinCurrency !== 'USD' &&
+              <Row>
+                <Col xs={8} md={8}>
+                  <h5> <b>Value of {this.state.coinCurrency} at time of transaction </b></h5>
+                  <FieldGroup
+                    id="formControlsText"
+                    type="text"
+                    placeholder="Amount"
+                  />
+                  <FormControl componentClass="select">
+                    <option value="select">USD</option>
+                    <option value="select">EUR</option>
+                  </FormControl>
+                </Col>
+              </Row>
+            }
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
+            <Button onClick={this.close}>Cancel</Button>
+            <Button bsStyle="success"> Save </Button>
           </Modal.Footer>
         </Modal>
         </Col>
